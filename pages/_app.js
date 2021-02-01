0cb545dcc8fbc978f04422eab83b7a2b21b3481b
Onesimus Addo <38097150+draddo11@users.noreply.github.com>
2021-02-01 23:43:01 +0000

    added files

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { Text } from '@chakra-ui/core';
+import { jsx } from '@emotion/react';
+
+function App() {
+  return (
+    <Text fontSize="40px" color="rebeccapurple" as="h1">
+      Hello, Herm!
+    </Text>
+  );
+}
+export default App;
