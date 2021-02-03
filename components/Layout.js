f6f9bc55bb38e8a58d0c36c0f57515ba834496fa
draddo11 <38097150+draddo11@users.noreply.github.com>
2021-02-03 22:16:44 +0000

    new setup

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box, Flex } from '@chakra-ui/core';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import Main from './Main';
+function Layout({ children }) {
+  return (
+    <Box>
+      <Header></Header>
+      <Flex>
+        <Sidebar></Sidebar>
+        <Main>{children}</Main>
+      </Flex>
+    </Box>
+  );
+}
+export default Layout;
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from '@chakra-ui/react';
+import Layout from '../components/Layout';
+
+function Index() {
+    return (
+
+    <Layout>
+      <Text fontSize="40px" color="brand.500" as="h1">
+        Hello, Herm!
+      </Text>
+    </Layout>
+    
+    );
+  }
+  
+export default Index;
